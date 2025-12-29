d8c997f3f5bc8070fe1c11b518cfa743736ddb58
CHAGANTI PRAVEEN SAI REDDY <chagantipraveensai@gmail.com>
2025-12-29 16:08:49 +0530

    Update Flight.java

--- Flight.java
+++ Flight.java
@@ -1,7 +1,29 @@
 package com.nt.entity;
 
-public final class Flight 
-{
-	
+import jakarta.annotation.Generated;
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Entity
+@Table(name = "flight")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Flight {
+
+
+@Id
+@Generated(strategy = GenerationType.IDENTITY)
+private Long flightId;
+
+
+private String flightName;
+private String source;
+private String destination;
+private Double price;
+}
+
 
 }
+
